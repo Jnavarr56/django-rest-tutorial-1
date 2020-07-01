2e7748315ef8c380c3adb8f27b0fdb646a3ce354
Jorge Navarro <jnavarr56@gmail.com>
2020-07-01 17:27:38 -0400

    serializers

--- djangoapi/courses/urls.py
+++ djangoapi/courses/urls.py
@@ -15,5 +15,12 @@ Including another URLconf
 """
 from django.urls import path, include
 from . import views
+from rest_framework import routers
 
-urlpatterns = []
+
+router = routers.DefaultRouter()
+router.register('courses', views.CourseView)
+
+urlpatterns = [
+    path('', include(router.urls))
+]
